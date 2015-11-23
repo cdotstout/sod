7fe0a4929ac1bc1703f19a07a4846c1655cbb41f
Carlos Pizano <cpu@chromium.org>
2015-11-23 15:54:43 -0800

    [dart][examples] Add a fiber / channel example.
    Can also be used to benchmark send / receive.
    
    TBR=scottmg@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1469233002 .

---
+++ dart/examples/channel/channel.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2015, the SoD project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+//
+
+import 'dart:fletch';
+
+const int MESSAGE_COUNT = 1000;
+
+void channelResponder(Channel output) {
+  Channel input = new Channel();
+  output.send(input);
+  int message;
+  do {
+    message = input.receive();
+    output.send(message - 1);
+  } while (message > 0);
+}
+
+void main() {
+  var input = new Channel();
+  Fiber.fork(() => channelResponder(input));
+  var output = input.receive();
+
+  int i = MESSAGE_COUNT;
+  while (i > 0) {
+    output.send(i);
+    i = input.receive();
+  }
+
+  output.send(0);
+}
+
+
