dfbf540d4ebfc6a28150c4dd6726aab447e479de
Carlos Pizano <cpu@chromium.org>
2015-12-12 13:14:26 -0800

    [app][fletch] Fix QEMU build.
    sensors.c was fubar for the A15 QEMU.
    
    BUG=
    TBR=travisg@google.com
    
    Review URL: https://codereview.chromium.org/1517423002 .

--- app/fletch/sensors.c
+++ app/fletch/sensors.c
@@ -2,23 +2,22 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
-#include <stdio.h>
-
-#include <arch/arm/cm.h>
-
 #include <kernel/port.h>
 
+#if !defined(TARGET_QEMU_VIRT)
+#include <dev/gpio.h>
+#include <arch/arm/cm.h>
 #include <platform/stm32.h>
 #include <platform/gpio.h>
-
-#include <dev/gpio.h>
-
 #include <target/gpioconfig.h>
 
 #define GPIO_EXTI_GET(line) (EXTI->PR & (line))
 #define GPIO_EXTI_CLEAR(line) (EXTI->PR = (line))
+#endif
 
+#define SWITCH_PORT_NAME  "sys/io/sw"
 port_t switch_port;
+
 int switch_ev_count;
 
 typedef enum {
@@ -33,6 +32,16 @@ static void Switch_Callback(switch_event_t ev) {
   port_write(switch_port, &packet, 1);
 }
 
+#if defined(TARGET_QEMU_VIRT)
+//////////////////////////////////////////////////////////////////////////////
+void SensorsInit(void) {
+  port_create(SWITCH_PORT_NAME, PORT_MODE_BROADCAST, &switch_port);
+  // Fake a button press.
+  Switch_Callback(SWITCH_1_TOGGLE);
+}
+
+#elif defined(TARGET_DARTUINOP0)
+/////////////////////////////////////////////////////////////////////////////
 void stm32_EXTI15_10_IRQ(void) {
   arm_cm_irq_entry();
   if (GPIO_EXTI_GET(GPIO_TO_PIN_MASK(GPIO_SW100)) != RESET) {
@@ -54,13 +63,20 @@ void stm32_EXTI15_10_IRQ(void) {
   arm_cm_irq_exit(true);
 }
 
-#if !defined(TARGET_DARTUINOP0)
 void SensorsInit(void) {
-  // TODO(cpu): Implement this for Disco board.
-}
-#else
-void SensorsInit(void) {
-  port_create("sys/io/sw", PORT_MODE_BROADCAST, &switch_port);
+  port_create(SWITCH_PORT_NAME, PORT_MODE_BROADCAST, &switch_port);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+}
+
+#else
+/////////////////////////////////////////////////////////////////////////////
+  // TODO(cpu): implement for Disco target.
+void stm32_EXTI15_10_IRQ(void) {
+  arm_cm_irq_entry();
+  arm_cm_irq_exit(true);   
+}
+
+void SensorsInit(void) {
+  port_create(SWITCH_PORT_NAME, PORT_MODE_BROADCAST, &switch_port);
 }
 #endif
