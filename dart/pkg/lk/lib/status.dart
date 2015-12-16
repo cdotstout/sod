314e9e3f59387a08490a15c63410803c04d27409
Carlos Pizano <cpu@chromium.org>
2015-12-16 10:40:40 -0800

    [dart] Add LKStatus class.
    BUG=
    R=ager@google.com
    
    Review URL: https://codereview.chromium.org/1528733003 .

---
+++ dart/pkg/lk/lib/status.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+library lk_status;
+
+const int NO_ERROR               =  0;
+const int ERR_GENERIC            = -1;  
+const int ERR_NOT_FOUND          = -2;
+const int ERR_NOT_READY          = -3;
+const int ERR_NO_MSG             = -4;
+const int ERR_NO_MEMORY          = -5;
+const int ERR_ALREADY_STARTED    = -6;
+const int ERR_NOT_VALID          = -7;
+const int ERR_INVALID_ARGS       = -8;
+const int ERR_NOT_ENOUGH_BUFFER  = -9;
+const int ERR_NOT_SUSPENDED      = -10;
+const int ERR_OBJECT_DESTROYED   = -11;
+const int ERR_NOT_BLOCKED        = -12;
+const int ERR_TIMED_OUT          = -13;
+const int ERR_ALREADY_EXISTS     = -14;
+const int ERR_CHANNEL_CLOSED     = -15;
+const int ERR_OFFLINE            = -16;
+const int ERR_NOT_ALLOWED        = -17;
+const int ERR_BAD_PATH           = -18;
+const int ERR_ALREADY_MOUNTED    = -19;
+const int ERR_IO                 = -20;
+const int ERR_NOT_DIR            = -21;
+const int ERR_NOT_FILE           = -22;
+const int ERR_RECURSE_TOO_DEEP   = -23;
+const int ERR_NOT_SUPPORTED      = -24;
+const int ERR_TOO_BIG            = -25;
+const int ERR_CANCELLED          = -26;
+const int ERR_NOT_IMPLEMENTED    = -27;
+const int ERR_CHECKSUM_FAIL      = -28;
+const int ERR_CRC_FAIL           = -29;
+const int ERR_CMD_UNKNOWN        = -30;
+const int ERR_BAD_STATE          = -31;
+const int ERR_BAD_LEN            = -32;
+const int ERR_BUSY               = -33;
+const int ERR_THREAD_DETACHED    = -34;
+const int ERR_I2C_NACK           = -35;
+const int ERR_ALREADY_EXPIRED    = -36;
+const int ERR_OUT_OF_RANGE       = -37;
+const int ERR_NOT_CONFIGURED     = -38;
+const int ERR_NOT_MOUNTED        = -39;
+const int ERR_FAULT              = -40;
+const int ERR_NO_RESOURCES       = -41;
+const int ERR_BAD_HANDLE         = -42;
+const int ERR_ACCESS_DENIED      = -43;
+const int ERR_PARTIAL_WRITE      = -44;
