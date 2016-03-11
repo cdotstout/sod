8e3f45faa7eb5ad67c819e7e0d39d80ad3e043c5
cstout <cstout@google.com>
2016-03-11 13:02:24 -0800

    [dartuino] Add LK_FPU=fpv4-sp-d16
    R=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/1786753002 .

--- project/dartuino-p0-dartino.mk
+++ project/dartuino-p0-dartino.mk
@@ -4,6 +4,6 @@ include project/virtual/fs.mk
 MODULES += app/dartino app/shell
 
 DARTINO_CONFIGURATION = LK
-DARTINO_GYP_DEFINES = "LK_PATH=../../third_party/lk/ LK_PROJECT=dartuino-p0-dartino LK_CPU=cortex-m4 LK_USE_DEPS_ARM_GCC=0"
+DARTINO_GYP_DEFINES = "LK_PATH=../../third_party/lk/ LK_PROJECT=dartuino-p0-dartino LK_CPU=cortex-m4 LK_FPU=fpv4-sp-d16 LK_USE_DEPS_ARM_GCC=0"
 
 WITH_CPP_SUPPORT=true
