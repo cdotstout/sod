c15fb282b00c10a1f58fcfdaab43e713ec368753
cstout <cstout@google.com>
2016-03-29 12:29:22 -0700

    [dartuino] Add LK_FLOAT-API=soft
    Use emulated floating point until LK build supports fpu.
    
    R=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/1826203002 .

--- project/dartuino-p0-dartino.mk
+++ project/dartuino-p0-dartino.mk
@@ -4,6 +4,7 @@ include project/virtual/fs.mk
 MODULES += app/dartino app/shell
 
 DARTINO_CONFIGURATION = LK
-DARTINO_GYP_DEFINES = "LK_PATH=../../third_party/lk/ LK_PROJECT=dartuino-p0-dartino LK_CPU=cortex-m4 LK_FPU=fpv4-sp-d16 LK_USE_DEPS_ARM_GCC=0"
+# Until lk enables FPU support, use soft (emulated) floating point.
+DARTINO_GYP_DEFINES = "LK_PATH=../../third_party/lk/ LK_PROJECT=dartuino-p0-dartino LK_CPU=cortex-m4 LK_FPU=fpv4-sp-d16 LK_FLOAT-ABI=soft LK_USE_DEPS_ARM_GCC=0"
 
 WITH_CPP_SUPPORT=true
